9f3ce5ba21f869625ba840f11d614d566002f5f4
mackyjava <adriana.jauregui.varela@gmail.com>
2017-04-08 22:26:48 -0500

    con if pasan dos

--- ejercicio-01/ejercicio-01.js
+++ ejercicio-01/ejercicio-01.js
@@ -1,21 +1,16 @@
 function isValidCard(creditCardNumber) {
     // Escribe tu código aquí
-    switch(typeof(creditCardNumber)){
-		case "undefined":
-			return "Ingresa un numero";
-		break;
-		case "string":
-        	return "Error de dato";
-		break;
-		case "array":
-			return "Error de dato";
-		break;
-	    case "boolean":
-			return "Error de dato";
-		break;
-	 }
-  
+    if(typeof(creditCardNumber)=="undefined")
+	{
+		 return "Ingresa un numero";
+	}
+     else if(typeof(creditCardNumber)!="undefined")
+     {
+	  return "Error de dato";
+     }
+	
 }
+
 
 // NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
 if (typeof exports !== 'undefined') {
