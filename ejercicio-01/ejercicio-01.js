603580aa792de25875a24f92430dba8cb215e665
mackyjava <adriana.jauregui.varela@gmail.com>
2017-04-09 15:01:15 -0500

    pasan tres casos y no se por que?

--- ejercicio-01/ejercicio-01.js
+++ ejercicio-01/ejercicio-01.js
@@ -1,16 +1,22 @@
+var result=0;
+
+
 function isValidCard(creditCardNumber) {
     // Escribe tu código aquí
     if(typeof(creditCardNumber)=="undefined")
 	{
 		 return "Ingresa un numero";
 	}
-     else if(typeof(creditCardNumber)!="undefined")
+     else if(typeof(creditCardNumber)!="number")
      {
 	  return "Error de dato";
      }
-	
-}
-
+	 else if(typeof(creditCardNumber)=="number"){
+		 result = creditCardNumber;
+	     var result1=Array.from(result);
+		     return result1;
+	     }
+     }
 
 // NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
 if (typeof exports !== 'undefined') {
